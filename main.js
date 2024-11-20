6f141af6c21aa02ca6e1a9930c35a15acd9f2408
luis vaquera <luisvd01@gmail.com>
2024-11-19 19:26:26 -0600

    mi primer commit

---
+++ main.js
@@ -0,0 +1,21 @@
+if ('serviceWorker' in navigator) {
+    console.log('Si tiene Service Worker');
+
+    navigator.serviceWorker.register('/sw.js')  // Asegúrate de que la ruta sea correcta
+        .then(res => console.log('Service Worker cargando correctamente', res))
+        .catch(err => console.log('Service Worker no se pudo cargar', err));  // Cambiado 'res' por 'err'
+} else {
+    console.log('No se localiza Service Worker');
+}
+
+if ("serviceWorker" in navigator) {
+    console.log("si tiene");
+  
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then((res) => console.log("serviceWorker cargado"))
+      .catch((err) => console.log("serviceWorker no se puede cargar"));
+  } else {
+    console.log("no se enceuntra");
+  }
+  
---
+++ sw.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = "v1_cache_PWA";
+
+// Archivos que se almacenarán en la caché
+var urlsToCache = [
+    './',
+    '/images/Logo-VaqueraShop 16x16.jpg',
+    '/images/Logo-VaqueraShop 32x32.jpg',
+    '/images/Logo-VaqueraShop 64x64.jpg',
+    '/images/Logo-VaqueraShop96x96.jpg',
+    '/images/Logo-VaqueraShop 128x128.jpg',
+    '/images/Logo-VaqueraShop 256x256.jpg',
+    '/images/Logo-VaqueraShop 384x384.jpg',
+    '/images/Logo-VaqueraShop 512x512.jpg',
+    '/images/Logo-VaqueraShop 1024x1024.jpg'
+];
+
+//instalacion del service worker
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache
+        .addAll(urlsToCache)
+        .then(() => {
+          self.skipWaiting();
+        })
+        .catch((err) => {
+          console.log("no se ha cargado la cache ", err);
+        });
+    })
+  );
+});
+
+// Evento Activate activa el Sw y permite que trabaje offline
+self.addEventListener("activate", (e) => {
+  //añadimos todos los elementos en la cache
+  const cacheWhiteList = [CACHE_NAME];
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhiteList.indexOf(cacheName) === -1) {
+              //borrar los elementos que ya no esten en la cache
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => {
+        //Activar cache en el dispositivo
+        self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        // devuelvo datos desde cache
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
